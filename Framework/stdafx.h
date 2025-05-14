25aec985ff6840abb18a051af62b0ad99847c15b
被子 <jgw2007@vip.qq.com>
2025-05-14 15:22:10 +0800

    Create stdafx.h

---
+++ Framework/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cassert>
+#include <cstdio>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <optional>
+
+#include <Volk/volk.h>
+#include <vulkan/vulkan.hpp>
