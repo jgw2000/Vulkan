a1d2d0d096ccc8e624afae84195b7ccb4da94de9
被子 <jgw2007@vip.qq.com>
2025-05-15 19:26:16 +0800

    Update hpp_physical_device.h

--- Framework/core/hpp_physical_device.h
+++ Framework/core/hpp_physical_device.h
@@ -53,6 +53,104 @@ namespace vkb::core
         vk::PhysicalDeviceFeatures& get_mutable_requested_features();
 
         /**
+         * @brief Get an extension features struct
+         *
+         *        Gets the actual extension features struct with the supported flags set.
+         *        The flags you're interested in can be set in a corresponding struct in the structure chain
+         *        by calling PhysicalDevice::add_extension_features()
+         * @returns The extension feature struct
+         */
+        template <typename HPPStructureType>
+        HPPStructureType get_extension_features()
+        {
+            // We cannot request extension features if the physical device properties 2 instance extension isn't enabled
+            if (!instance.is_enabled(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME))
+            {
+                throw std::runtime_error("Couldn't request feature from device as " +
+                      std::string(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME) + " isn't enabled!");
+            }
+
+            // Get the extension feature
+            return handle.getFeatures2KHR<vk::PhysicalDeviceFeatures2KHR, HPPStructureType>().template get<HPPStructureType>();
+        }
+
+        /**
+         * @brief Add an extension features struct to the structure chain used for device creation
+         *
+         *        To have the features enabled, this function must be called before the logical device
+         *        is created. To do this request sample specific features inside
+         *        VulkanSample::request_gpu_features(vkb::HPPPhysicalDevice &gpu).
+         *
+         *        If the feature extension requires you to ask for certain features to be enabled, you can
+         *        modify the struct returned by this function, it will propagate the changes to the logical
+         *        device.
+         * @returns A reference to the extension feature struct in the structure chain
+         */
+        template <typename HPPStructureType>
+        HPPStructureType& add_extension_features()
+        {
+            // We cannot request extension features if the physical device properties 2 instance extension isn't enabled
+            if (!instance.is_enabled(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME))
+            {
+                throw std::runtime_error("Couldn't request feature from device as " +
+                      std::string(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME) + " isn't enabled!");
+            }
+
+            // Add an (empty) extension features into the map of extension features
+            auto [it, added] = extension_features.insert({ HPPStructureType::structureType, std::make_shared<HPPStructureType>() });
+            if (added)
+            {
+                // if it was actually added, also add it to the structure chain
+                if (last_requested_extension_feature)
+                {
+                    static_cast<HPPStructureType*>(it->second.get())->pNext = last_requested_extension_feature;
+                }
+                last_requested_extension_feature = it->second.get();
+            }
+
+            return *static_cast<HPPStructureType*>(it->second.get());
+        }
+
+        /**
+         * @brief Request an optional features flag
+         *
+         *        Calls get_extension_features to get the support of the requested flag. If it's supported,
+         *        add_extension_features is called, otherwise a log message is generated.
+         *
+         * @returns true if the requested feature is supported, otherwise false
+         */
+        template <typename Feature>
+        vk::Bool32 request_optional_feature(vk::Bool32 Feature::* flag, std::string const& featureName, std::string const& flagName)
+        {
+            vk::Bool32 supported = get_extension_features<Feature>().*flag;
+            if (supported)
+            {
+                add_extension_features<Feature>().*flag = true;
+            }
+
+            return supported;
+        }
+
+        /**
+         * @brief Request a required features flag
+         *
+         *        Calls get_extension_features to get the support of the requested flag. If it's supported,
+         *        add_extension_features is called, otherwise a runtime_error is thrown.
+         */
+        template <typename Feature>
+        void request_required_feature(vk::Bool32 Feature::* flag, const std::string& featureName, const std::string& flagName)
+        {
+            if (get_extension_features<Feature>().*flag)
+            {
+                add_extension_features<Feature>().*flag = true;
+            }
+            else
+            {
+                throw std::runtime_error(std::string("Requested required feature <") + featureName + "::" + flagName + "> is not supported");
+            }
+        }
+
+        /**
          * @brief Sets whether or not the first graphics queue should have higher priority than other queues.
          * Very specific feature which is used by async compute samples.
          * @param enable If true, present queue will have prio 1.0 and other queues have prio 0.5.
