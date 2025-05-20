18d57a2f89fbf26f647e0f260b8aefad58fb4ec8
被子 <jgw2007@vip.qq.com>
2025-05-20 15:49:53 +0800

    Update vulkan_resource.cpp

--- Framework/core/vulkan_resource.cpp
+++ Framework/core/vulkan_resource.cpp
@@ -1,19 +1,1 @@
 #include "stdafx.h"
-#include "vulkan_resource.h"
-
-namespace vkb::core
-{
-    template <typename Handle>
-    VulkanResource<Handle>::VulkanResource(VulkanResource&& other) :
-        handle(std::exchange(other.handle, {})),
-        device(std::exchange(other.device, {}))
-    {}
-
-    template <typename Handle>
-    VulkanResource<Handle>& VulkanResource<Handle>::operator=(VulkanResource&& other)
-    {
-        handle = std::exchange(other.handle, {});
-        device = std::exchange(other.device, {});
-        return *this;
-    }
-}
